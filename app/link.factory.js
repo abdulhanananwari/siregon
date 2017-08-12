428d95fe056433f27f04118a9a9df17e28b9eca0
abdulhanan <abdulhananx42@gmail.com>
2017-08-12 20:44:44 +0700

    first commit

---
+++ app/link.factory.js
@@ -0,0 +1,11 @@
+app
+	.factory('LinkFactory', function(){
+		var domains {
+			siregon: window.location.protocol + '//' + window.location.host + '/',	
+		}
+		var apps = {
+			siregon : domains.siregon + 'registrations',
+
+		} ;
+
+	});
